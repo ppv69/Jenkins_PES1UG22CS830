ddc3b02dc703a88b4766f233a7890b23404024e8
ppv69 <prithvivasistha@gmail.com>
2024-03-06 16:33:24 +0530

    Jenkinsfile Added

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,35 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                sh 'g++ PES1UG22CS830_Task_5.cpp -o temp'
+                 build job: 'PES1UG22CS830_Task_5', wait: false
+                 echo 'Build Successful'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                sh 'cat PES1UG22CS830_Task_5.cpp'
+                echo 'Test Successful'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+               
+                 echo 'Deploy Successful'
+            }
+        }
+    }
+
+    post {
+        failure {
+            
+                echo 'Pipeline Failed'
+          
+        }
+    }
+}
